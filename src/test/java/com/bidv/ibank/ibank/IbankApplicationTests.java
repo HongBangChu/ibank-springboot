e3d241c8c426accf119e8eb43654384fd8700c03
Hong Bang Chu <minhnh.2.1@gmail.com>
2022-03-07 16:43:23 +0700

    init

---
+++ src/test/java/com/bidv/ibank/ibank/IbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidv.ibank.ibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
